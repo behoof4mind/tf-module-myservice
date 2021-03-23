777f95a44f43d34711e31e90c10733719794cba7
Denis <denis.lavrushko@infobip.com>
2021-03-23 19:38:23 +0300

    upd

--- web/main.tf
+++ web/main.tf
@@ -1,41 +1,97 @@
 
+terraform {
+  required_version = ">= 0.12.26"
+}
+
+data "aws_availability_zones" "all" {}
+
+resource "aws_autoscaling_group" "myservice" {
+  launch_configuration = aws_launch_configuration.myservice.id
+  availability_zones   = data.aws_availability_zones.all.names
+
+  min_size = 2
+  max_size = 10
+
+  load_balancers    = [aws_elb.myservice.name]
+  health_check_type = "ELB"
+
+  tag {
+    key                 = "Name"
+    value               = "terraform-asg-example"
+    propagate_at_launch = true
+  }
+}
+
+resource "aws_launch_configuration" "myservice" {
+  image_id        = "ami-0c55b159cbfafe1f0"
+  instance_type   = "t2.micro"
+  security_groups = [aws_security_group.web-instance.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p "${var.server_port}" &
+              EOF
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_security_group" "web-instance" {
+  name = "terraform-myservice-instance"
+
+  ingress {
+    from_port   = var.server_port
+    to_port     = var.server_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_elb" "myservice" {
+  name               = "terraform-asg"
+  security_groups    = [aws_security_group.elb.id]
+  availability_zones = data.aws_availability_zones.all.names
+
+  health_check {
+    target              = "HTTP:${var.server_port}/"
+    interval            = 30
+    timeout             = 3
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+
+  listener {
+    lb_port           = var.elb_port
+    lb_protocol       = "http"
+    instance_port     = var.server_port
+    instance_protocol = "http"
+  }
+}
+
+resource "aws_security_group" "elb" {
+  name = "myservice-elb"
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = var.elb_port
+    to_port     = var.elb_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_vpc" "web_vpc" {
   cidr_block = "172.16.0.0/16"
 
   tags = {
     Name = "myservice"
-  }
-}
-
-resource "aws_subnet" "web_subnet" {
-  vpc_id            = aws_vpc.web_vpc.id
-  cidr_block        = "172.16.10.0/24"
-  availability_zone = "us-east-2a"
-
-  tags = {
-    Name = "myservice"
-  }
-}
-
-resource "aws_network_interface" "web" {
-  subnet_id   = aws_subnet.web_subnet.id
-  private_ips = ["172.16.10.100"]
-
-  tags = {
-    Name = "primary_network_interface"
-  }
-}
-
-resource "aws_instance" "web" {
-  ami           = "ami-08962a4068733a2b6"
-  instance_type = "t2.micro"
-
-  network_interface {
-    network_interface_id = aws_network_interface.web.id
-    device_index         = 0
-  }
-
-  credit_specification {
-    cpu_credits = "unlimited"
   }
 }
