54566b758e2c07aa4de193569409a47d44e1f5b3
Denis <denis.lavrushko@infobip.com>
2021-03-23 22:18:13 +0300

    removed backend.tf

--- web/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "terraform-my-service-state"
-    key    = "terraform.tfstate"
-    dynamodb_table = "terraform-my-service-state"
-    encrypt        = true
-  }
-}
