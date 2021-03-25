907f84a5dc906fbeb47ecfb1ea8bbfcc40b25a86
Denis <denis.lavrushko@infobip.com>
2021-03-25 21:04:06 +0300

    added vpc to security groups

--- security-groups.tf
+++ security-groups.tf
@@ -44,9 +44,10 @@ resource "aws_security_group" "ssh-access" {
     cidr_blocks = ["0.0.0.0/0"]
   }
   egress {
-    from_port = 0
-    to_port = 0
-    protocol = "tcp"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
